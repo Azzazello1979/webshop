75803f4378c0975fb6a97e5d596dd9e3a0782e1d
Azzazello1979 <balint.haui@gmail.com>
2019-08-29 18:21:04 +0200

    so nice

--- frontEnd/webshop/src/app/app.module.ts
+++ frontEnd/webshop/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavibarComponent } from './navibar/navibar.component';
 import { LoginLogoutComponent } from './login-logout/login-logout.component';
+import { MainComponent } from './main/main.component';
+import { LeftComponent } from './left/left.component';
+import { RightComponent } from './right/right.component';
+import { MidComponent } from './mid/mid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavibarComponent,
-    LoginLogoutComponent
+    LoginLogoutComponent,
+    MainComponent,
+    LeftComponent,
+    RightComponent,
+    MidComponent
   ],
   imports: [
     BrowserModule,
