aca20f1ba1a1fc492ae18c60ed4b362d5937a362
Azzazello1979 <balint.haui@gmail.com>
2019-10-04 11:03:04 +0200

    login-logout component rxjs Subscription/unsubscribe added to userIntent() method

--- frontEnd/webshop/src/app/components/login-logout/login-logout.component.ts
+++ frontEnd/webshop/src/app/components/login-logout/login-logout.component.ts
@@ -1,8 +1,9 @@
 import { AuthService } from './../../services/auth.service';
 import { CartService } from './../../services/cart.service';
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Router} from '@angular/router';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -10,10 +11,12 @@ import { Router} from '@angular/router';
   templateUrl: './login-logout.component.html',
   styleUrls: ['./login-logout.component.css']
 })
-export class LoginLogoutComponent implements OnInit {
+export class LoginLogoutComponent implements OnDestroy {
 
   buttonText: string = 'Login'; //initial value
   userObject: any = {};
+  registerSubscription:Subscription = new Subscription();
+  loginSubscription:Subscription = new Subscription();
 
 
   constructor(
@@ -27,14 +30,14 @@ export class LoginLogoutComponent implements OnInit {
     this.auth.toggle();
   }
 
-  registerORloginORlogout() {
+  userIntent() { // register? login? logout?
     if(this.auth.hasToken()){
       this.auth.logout(); // call logout service
       this.cartService.clearCart(); // empty cart on logout
       this.router.navigate(['/landingpage']);
       
     } else if(this.auth.wantsToRegister === true) { //call register service
-      this.auth.register(this.userObject)
+      this.registerSubscription = this.auth.register(this.userObject)
         .subscribe(
           (endPointResponseObj) => {
           localStorage.setItem('token', endPointResponseObj.token);
@@ -46,7 +49,7 @@ export class LoginLogoutComponent implements OnInit {
         );
         
     } else if(this.auth.wantsToRegister === false){ //call login service
-      this.auth.logIn(this.userObject)
+      this.loginSubscription = this.auth.logIn(this.userObject)
       .subscribe(
         (endPointResponseObj) => {
         localStorage.setItem('token', endPointResponseObj.token);
@@ -61,19 +64,11 @@ export class LoginLogoutComponent implements OnInit {
     this.userObject = {};
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  ngOnInit() {}
+  // prevent mem. leaks, without this, component is destroyed on navigating away but the
+  // observable<-->observer lives on ... ---> use async pipe !!! 
+  ngOnDestroy(){  
+    this.registerSubscription.unsubscribe();
+    this.loginSubscription.unsubscribe();
+  }
 
 }
