7a0f423531f7114ba9c2530bb0357febb2dc8366
Balint H <balint.haui@gmail.com>
2019-10-15 00:02:17 +0200

    little housekeeping

--- frontEnd/webshop/src/app/app-routing.module.ts
+++ frontEnd/webshop/src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
-import { CheckoutComponent } from './components/checkout/checkout.component';
-import { MenuComponent } from './components/menu/menu.component';
-import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { CartComponent } from './components/cart/cart.component';
+import { AddressComponent } from './components/address/address.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { PaymentComponent } from './components/payment/payment.component';
+import { SuccessComponent } from './components/success/success.component';
+import { MenuComponent } from './components/menu/menu.component';
+
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { MainComponent } from './components/main/main.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth.guard';
 import { LandingpageComponent } from './components/landingpage/landingpage.component';
-import { AddressComponent } from './components/address/address.component';
-import { PaymentComponent } from './components/payment/payment.component';
-import { SuccessComponent } from './components/success/success.component';
 
 
 
@@ -17,12 +18,12 @@ const routes: Routes = [
   { path: '', redirectTo: 'landingpage', pathMatch: 'full' },
   { path: 'landingpage', component: LandingpageComponent },
   { path: 'dashboard', component: MainComponent, canActivate: [AuthGuard] },
-  { path: 'menu', component: MenuComponent, canActivate: [AuthGuard] },
   { path: 'cart', component: CartComponent, canActivate: [AuthGuard]  },
   { path: 'address', component: AddressComponent, canActivate: [AuthGuard] },
   { path: 'checkout', component: CheckoutComponent, canActivate: [AuthGuard] },
   { path: 'payment', component: PaymentComponent, canActivate: [AuthGuard] },
   { path: 'success', component: SuccessComponent, canActivate: [AuthGuard] },
+  { path: 'menu', component: MenuComponent, canActivate: [AuthGuard] },
   { path: 'products/:productID', component: ProductDetailComponent, canActivate: [AuthGuard] }
 ];
 
