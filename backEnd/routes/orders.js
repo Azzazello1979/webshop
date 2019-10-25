e2daac44e12bace1b1578ea5ddbac68b204a60d4
Balint H <balint.haui@gmail.com>
2019-10-25 22:22:04 +0200

    address table is now a child of orders table (and not a child of users table) as it should be

--- backEnd/routes/orders.js
+++ backEnd/routes/orders.js
@@ -10,6 +10,7 @@ router.post('/', tokenControl, (req, res) => {
     res.setHeader('Content-Type', 'application/json');
 
     let currentUserID;
+    let orderID;
 
     let validatedZIP;
     let validatedPOBOX;
@@ -45,13 +46,15 @@ router.post('/', tokenControl, (req, res) => {
         }).
 
 
-        // insert record(s) into suborder table, order_id === orders.id    
+        // insert record(s) into suborder table 
         then((response2) => {
             console.log('response after INSERT INTO orders: ', response2)
 
+            orderID = response2[0].insertId; // orderID to be used in suborder table, address table and billing_address table
+
             req.body.products.forEach(e => {
                 return db.query(`INSERT INTO suborder (order_id, product_id, amount) VALUES 
-            ( ${response2[0].insertId}, ${e.id}, ${e.amount} );`)
+            ( ${orderID}, ${e.id}, ${e.amount} );`)
             });
 
         }).
@@ -60,8 +63,8 @@ router.post('/', tokenControl, (req, res) => {
         // insert record into ADDRESS table
         then((response3) => {
             console.log('response after INSERT INTO suborder: ', response3)
-            return db.query(`INSERT INTO address (user_id, name, country, state, county, city, ZIP, POBOX, address1, address2, extra) VALUES 
-        ( ${currentUserID}, '${req.body.shippingAddress.name}', '${req.body.shippingAddress.country}', '${req.body.shippingAddress.state}', 
+            return db.query(`INSERT INTO address (order_id, name, country, state, county, city, ZIP, POBOX, address1, address2, extra) VALUES 
+        ( ${orderID}, '${req.body.shippingAddress.name}', '${req.body.shippingAddress.country}', '${req.body.shippingAddress.state}', 
         '${req.body.shippingAddress.county}', '${req.body.shippingAddress.city}', ${validatedZIP}, 
         ${validatedPOBOX}, '${req.body.shippingAddress.address1}', '${req.body.shippingAddress.address2}', 
         '${req.body.shippingAddress.extra}' );`)
@@ -73,8 +76,8 @@ router.post('/', tokenControl, (req, res) => {
             console.log('response after INSERT INTO address: ', response4)
             if (req.body.billingAddress)
 
-                return db.query(`INSERT INTO billing_address (user_id, name, country, state, county, city, ZIP, POBOX, address1, address2) VALUES 
-        ( ${currentUserID}, '${req.body.billingAddress.name}', '${req.body.billingAddress.country}', '${req.body.billingAddress.state}', 
+                return db.query(`INSERT INTO billing_address (order_id, name, country, state, county, city, ZIP, POBOX, address1, address2) VALUES 
+        ( ${orderID}, '${req.body.billingAddress.name}', '${req.body.billingAddress.country}', '${req.body.billingAddress.state}', 
         '${req.body.billingAddress.county}', '${req.body.billingAddress.city}', ${validated_b_ZIP}, 
         ${validated_b_POBOX}, '${req.body.billingAddress.address1}', '${req.body.billingAddress.address2}' );`)
 
