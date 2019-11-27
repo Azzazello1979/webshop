991a5fef6ae2a5abeed71a1683750c89394f9ee7
Balint H <balint.haui@gmail.com>
2019-11-27 14:51:26 +0100

    products must load from db 1st

--- backEnd/routes/orders.js
+++ backEnd/routes/orders.js
@@ -93,4 +93,29 @@ router.post("/", tokenControl, (req, res) => {
     });
 });
 
+/*
+router.get("/", tokenControl, (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  let decodedToken = jwt.decode(req.headers.authorization.split(" ")[1]);
+  let currentUserID = decodedToken.id;
+
+  try {
+    getPreviousOrders();
+  } catch (err) {
+    return console.log("ERROR @ cart backend @ getPreviousOrders() " + err);
+  }
+
+  async function getPreviousOrders(){
+    let response = await db.query(
+      `
+      
+      `
+    );
+
+    res.status(200).send(response);
+  }
+
+});
+*/
+
 module.exports = router;
