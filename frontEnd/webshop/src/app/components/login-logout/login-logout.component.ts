eec5856650568ad93d44cc7f9623920d23cb4d34
Balint H <balint.haui@gmail.com>
2020-02-02 17:43:29 +0100

    login-logout.component userIntent() method split to smaller pieces

--- frontEnd/webshop/src/app/components/login-logout/login-logout.component.ts
+++ frontEnd/webshop/src/app/components/login-logout/login-logout.component.ts
@@ -17,9 +17,9 @@ export class LoginLogoutComponent implements OnDestroy {
   userObject:UserObject = {
     'email': "",
     'password': ""
-  }
-  registerSubscription: Subscription = new Subscription()
-  loginSubscription: Subscription = new Subscription()
+  };
+  registerSubscription: Subscription = new Subscription();
+  loginSubscription: Subscription = new Subscription();
 
   constructor(
     private auth: AuthService,
@@ -32,84 +32,86 @@ export class LoginLogoutComponent implements OnDestroy {
     this.auth.toggle();
   }
 
+  logout(){
+    this.cartService.saveCart();
+    this.router.navigate(["/landingpage"]);
+  }
+
+  register(registerLoginForm){
+    if(!registerLoginForm.valid){
+      console.log("Email and password is needed, they cannot be empty")
+      return window.alert("Email and password is needed, they cannot be empty")
+    } else if( this.userObject.email.split("").includes(" ") || this.userObject.password.split("").includes(" ") ) {
+      console.log("email or password cannot have space")
+      return window.alert("email or password cannot have space")
+    }
+
+    this.registerSubscription = this.auth.register(this.userObject)
+    .subscribe(
+      endPointResponseObj => {
+        localStorage.setItem("token", endPointResponseObj.token);
+        this.auth.loggedIn = true;
+        this.auth.buttonText = "Logout";
+        this.productService.getProductsFromDB();
+        this.router.navigate(["/dashboard"]);
+      },
+      error => {
+        console.log(error);
+        this.auth.loggedIn = false;
+        this.auth.buttonText = "Login";
+        window.alert("That email is taken, please choose a different one!");
+        return this.router.navigate(["/landingpage"]);
+      }
+    )
+  }
+
+  login(registerLoginForm){
+    if(!registerLoginForm.valid){
+      console.log("email and password is needed")
+      return window.alert("email and password is needed")
+    }
+
+    this.loginSubscription = this.auth.logIn(this.userObject)
+    .subscribe(
+      endPointResponseObj => {
+          localStorage.setItem("token", endPointResponseObj.token);
+          this.auth.loggedIn = true;
+          this.auth.buttonText = "Logout";
+          this.productService.getProductsFromDB();
+
+        // regular user or admin?
+        if(this.auth.adminLoggedIn){
+          this.router.navigate(["/admin"]);
+        } else {
+          this.cartService.initShippingOptions();
+          this.cartService.loadUserWish();
+          this.router.navigate(["/dashboard"]);
+      }
+        },
+      error => {
+        console.log(error)
+        this.auth.loggedIn = false
+        this.auth.buttonText = "Login"
+        window.alert("The supplied email or password is wrong!")
+        return this.router.navigate(["/landingpage"])
+      }
+    )
+  }
+
   // register? login? logout? are you the admin?
-  // data validation not implemented in template driven form on purpose(practice makeshift data validation)
   userIntent(registerLoginForm: NgForm) {
 
-    this.userObject.email = registerLoginForm.value.email
-    this.userObject.password = registerLoginForm.value.password
+    this.userObject.email = registerLoginForm.value.email;
+    this.userObject.password = registerLoginForm.value.password;
 
-    // call logout service
-    if (this.auth.hasToken()) {
-      this.cartService.saveCart();
-      this.router.navigate(["/landingpage"]);
+    this.auth.hasToken() ? this.logout() :
+      this.auth.wantsToRegister ? this.register(registerLoginForm) : this.login(registerLoginForm);
 
-    //call register service
-    } else if (this.auth.wantsToRegister) {
-      if (!registerLoginForm.valid){
-        console.log(" Email and password is needed, they cannot be empty ")
-        return window.alert(" Email and password is needed, they cannot be empty ")
-      } else if (
-        this.userObject.email.split("").includes(" ") ||
-        this.userObject.password.split("").includes(" ")
-      ) {
-        console.log(" email or password cannot have space ")
-        return window.alert(" email or password cannot have space ")
-      }
-      this.registerSubscription = this.auth.register(this.userObject)
-      .subscribe(
-        endPointResponseObj => {
-          localStorage.setItem("token", endPointResponseObj.token)
-          this.auth.loggedIn = true
-          this.auth.buttonText = "Logout"
-          this.productService.getProductsFromDB()
-          
-        },
-        error => {
-          console.log(error)
-          this.auth.loggedIn = false
-          this.auth.buttonText = "Login"
-          window.alert("That email is taken, please choose a different one!")
-          return this.router.navigate(["/landingpage"])
-        }
-      )
-
-      //call login service
-    } else if(!this.auth.wantsToRegister){
-      if(!registerLoginForm.valid){
-        console.log(" email and password is needed ")
-        return window.alert(" email and password is needed ")
-      }
-      this.loginSubscription = this.auth.logIn(this.userObject)
-      .subscribe(
-        endPointResponseObj => {
-            localStorage.setItem("token", endPointResponseObj.token);
-            this.auth.loggedIn = true;
-            this.auth.buttonText = "Logout";
-            this.productService.getProductsFromDB();
-
-          // regular user or admin?
-          if(this.auth.adminLoggedIn){
-            this.router.navigate(["/admin"]);
-          } else {
-            this.cartService.initShippingOptions();
-            this.cartService.loadUserWish();
-            this.router.navigate(["/dashboard"]);
-        }
-          },
-        error => {
-          console.log(error)
-          this.auth.loggedIn = false
-          this.auth.buttonText = "Login"
-          window.alert("The supplied email or password is wrong!")
-          return this.router.navigate(["/landingpage"])
-        }
-      )
-    }
     this.userObject = {
       'email': "",
       'password': ""
     }
+
   }
     
   ngOnDestroy() {
